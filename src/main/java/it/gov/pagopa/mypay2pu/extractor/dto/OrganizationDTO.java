3b01755278d3d2da01e3db5bd65f830c0e112a1c
macacia <marco.cacia@eng.it>
2026-07-09 10:11:47 +0200

    dto

--- src/main/java/it/gov/pagopa/mypay2pu/extractor/dto/OrganizationDTO.java
+++ src/main/java/it/gov/pagopa/mypay2pu/extractor/dto/OrganizationDTO.java
@@ -1,11 +1,17 @@
 package it.gov.pagopa.mypay2pu.extractor.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 public record OrganizationDTO(
+  @NotBlank
   String ipaCode,
   String externalOrganizationId,
+  @NotBlank
   String orgFiscalCode,
+  @NotBlank
   String orgName,
   String orgTypeCode,
   String orgEmail,
@@ -14,10 +20,13 @@ public record OrganizationDTO(
   String segregationCode,
   String cbillInterBankCode,
   String orgLogo,
+  @NotBlank
   String status,
   String additionalLanguage,
   LocalDate startDate,
+  @NotNull
   Boolean flagNotifyIo,
+  @NotNull
   Boolean flagNotifyOutcomePush
 ) {
 }
