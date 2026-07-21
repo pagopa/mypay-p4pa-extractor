1b1d6768d0c5d27fd7cf9ab572d4bf660585f24f
macacia <marco.cacia@eng.it>
2026-07-21 11:49:34 +0200

    fix facade

--- src/main/java/it/gov/pagopa/mypay2pu/extractor/service/DataExportFacadeService.java
+++ src/main/java/it/gov/pagopa/mypay2pu/extractor/service/DataExportFacadeService.java
@@ -4,20 +4,25 @@ import it.gov.pagopa.mypay2pu.extractor.dto.ExportFileResult;
 import it.gov.pagopa.mypay2pu.extractor.dto.generated.ExtractionRequest;
 import it.gov.pagopa.mypay2pu.extractor.exception.ExportFileTypeNotSupportedException;
 import it.gov.pagopa.mypay2pu.extractor.service.export.organization.OrganizationExportProcessingService;
+import it.gov.pagopa.mypay2pu.extractor.service.export.orgsil.OrgSilServiceExportProcessingService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DataExportFacadeService {
 
   private final OrganizationExportProcessingService organizationExportProcessingService;
+  private final OrgSilServiceExportProcessingService orgSilServiceExportProcessingService;
 
-  public DataExportFacadeService(OrganizationExportProcessingService organizationExportProcessingService) {
+  public DataExportFacadeService(OrganizationExportProcessingService organizationExportProcessingService,
+                                 OrgSilServiceExportProcessingService orgSilServiceExportProcessingService) {
     this.organizationExportProcessingService = organizationExportProcessingService;
+    this.orgSilServiceExportProcessingService = orgSilServiceExportProcessingService;
   }
 
   public ExportFileResult executeExport(String extractionId, ExtractionRequest request) {
     return switch (request.getFileTypes()) {
       case ORGANIZATIONS -> organizationExportProcessingService.executeExport(extractionId, request);
+      case ORG_SIL_SERVICES -> orgSilServiceExportProcessingService.executeExport(extractionId, request);
       default ->
         throw new ExportFileTypeNotSupportedException("Invalid export file type: " + request.getFileTypes());
     };
