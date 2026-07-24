fdd907a89a8eb0987203eed23bfc95695ae47bed
macacia <marco.cacia@eng.it>
2026-07-24 12:55:17 +0200

    some fix

--- src/main/resources/db/mypay/debt-position-type-org/debt-position-type-org.sql
+++ src/main/resources/db/mypay/debt-position-type-org/debt-position-type-org.sql
@@ -1,21 +1,21 @@
 SELECT
-    e.cod_ipa_ente AS ente_ipa_code,
-    etd.de_bilancio_default AS bilancio_default,
-    etd.cod_tipo AS codice,
-    etd.de_tipo AS descrizione,
-    etd.iban_accredito_psp AS cod_iban,
-    etd.iban_accredito_pi AS iban_postale,
-    etd.cod_conto_corrente_postale AS codice_conto_postale,
-    etd.de_intestatario_cc_postale AS intestatario_conto_postale,
-    etd.de_settore_ente AS settore_organizzazione,
-    CAST(ROUND(etd.importo * 100) AS BIGINT) AS importo_centesimi,
-    etd.de_url_pagamento_dovuto AS url_pagamento_esterno,
-    etd.flg_cf_anonimo::text AS codice_fiscale_anonimo,
-    etd.flg_scadenza_obbligatoria::text AS scadenza_obbligatoria,
-    etd.spontaneo::text AS pagamento_spontaneo,
-    etd.flg_notifica_io::text AS notifica_io,
-    etd.flg_attivo::text AS attivo,
-    etd.flg_notifica_esito_push::text AS notifica_esito_push,
+    e.cod_ipa_ente AS ipa_code,
+    etd.de_bilancio_default AS balance,
+    etd.cod_tipo AS code,
+    etd.de_tipo AS description,
+    etd.iban_accredito_psp AS iban,
+    etd.iban_accredito_pi AS postal_iban,
+    etd.cod_conto_corrente_postale AS postal_account_code,
+    etd.de_intestatario_cc_postale AS holder_postal_cc,
+    etd.de_settore_ente AS org_sector,
+    CAST(ROUND(etd.importo * 100) AS BIGINT) AS amount_cents,
+    etd.de_url_pagamento_dovuto AS external_payment_url,
+    etd.flg_cf_anonimo::text AS flag_anonymous_fiscal_code,
+    etd.flg_scadenza_obbligatoria::text AS flag_mandatory_due_date,
+    etd.spontaneo::text AS flag_spontaneous,
+    etd.flg_notifica_io::text AS flag_notify_io,
+    etd.flg_attivo::text AS flag_active,
+    etd.flg_notifica_esito_push::text AS flag_notify_outcome_push,
     (
         SELECT es.nome_applicativo
         FROM mygov_ente_sil es
@@ -23,17 +23,17 @@ SELECT
           AND etd.flg_notifica_esito_push = TRUE
         ORDER BY es.nome_applicativo ASC
         LIMIT 1
-    ) AS cod_serv_notifica_esito_push,
+    ) AS notify_outcome_push_org_sil_service_code,
     CASE
         WHEN etd.url_notifica_pnd IS NOT NULL
          AND etd.user_pnd IS NOT NULL
          AND etd.psw_pnd IS NOT NULL
         THEN 'true'
         ELSE 'false'
-    END AS attualizzazione_importo,
-    etd.codice_servizio AS codice_servizio,
-    etd.cod_xsd_causale AS cod_xsd_causale,
-    etd.url_notifica_pnd AS url_notifica_pnd,
+    END AS flag_amount_actualization,
+    etd.codice_servizio AS service_code,
+    etd.cod_xsd_causale AS spontaneous_form_code,
+    etd.url_notifica_pnd AS auth_url,
     etd.user_pnd AS user_pnd,
     etd.psw_pnd AS psw_pnd
 FROM mygov_ente_tipo_dovuto etd
