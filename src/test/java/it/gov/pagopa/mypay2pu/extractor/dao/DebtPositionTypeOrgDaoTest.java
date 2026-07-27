1e8daa230b9357aba0509b31ca397bdf789e315a
macacia <marco.cacia@eng.it>
2026-07-27 18:36:37 +0200

    fix test

--- src/test/java/it/gov/pagopa/mypay2pu/extractor/dao/DebtPositionTypeOrgDaoTest.java
+++ src/test/java/it/gov/pagopa/mypay2pu/extractor/dao/DebtPositionTypeOrgDaoTest.java
@@ -163,14 +163,14 @@ class DebtPositionTypeOrgDaoTest {
       "IPA1", "BILANCIO", "TAX", "Tax", "IT60X0542811101000000123456",
       "IT60X0542811101000000123456", "123456", "Municipality", "Public administration",
       1234L, "https://example.test/pay", false, true, true, false, false, true,
-      true, "PAYMENT_NOTIFICATION", true, "SERVICE", "XSD");
+      true, "PAYMENT_NOTIFICATION", true, "SERVICE", "APP_NAME", "XSD");
   }
 
   private DebtPositionTypeOrg sourceRowWithNullableValues() {
     return new DebtPositionTypeOrg(
       "IPA1", null, "FEE", "Fee", "IT60X0542811101000000123456",
       null, null, null, null, null, null, false, false, false, false, false, true,
-      false, null, false, null, null
+      false, null, false, null, null, null
     );
   }
 }
