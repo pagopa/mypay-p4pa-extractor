4840e36e1c11d028cc198503a5f0f51b51a76a23
macacia <marco.cacia@eng.it>
2026-07-29 12:17:20 +0200

    service skeleton

---
+++ src/main/java/it/gov/pagopa/mypay2pu/extractor/service/export/debtpositionstypeorgoperators/DebtPositionsTypeOrgOperatorsExportProcessingService.java
@@ -0,0 +1,69 @@
+package it.gov.pagopa.mypay2pu.extractor.service.export.debtpositionstypeorgoperators;
+
+import it.gov.pagopa.mypay2pu.extractor.config.ExtractorExportProperties;
+import it.gov.pagopa.mypay2pu.extractor.dao.DebtPositionsTypeOrgOperatorsDao;
+import it.gov.pagopa.mypay2pu.extractor.dto.export.PuDebtPositionsTypeOrgOperatorsDTO;
+import it.gov.pagopa.mypay2pu.extractor.dto.generated.ExtractionFilters;
+import it.gov.pagopa.mypay2pu.extractor.dto.generated.ExtractionRequest;
+import it.gov.pagopa.mypay2pu.extractor.dto.generated.MigrationFileType;
+import it.gov.pagopa.mypay2pu.extractor.mapper.debtpositionstypeorgoperators.DebtPositionsTypeOrgOperatorsMapper;
+import it.gov.pagopa.mypay2pu.extractor.model.mp4.DebtPositionsTypeOrgOperators;
+import it.gov.pagopa.mypay2pu.extractor.service.FileArchiverService;
+import it.gov.pagopa.mypay2pu.extractor.service.export.BaseExportProcessingService;
+import it.gov.pagopa.mypay2pu.extractor.service.export.CsvPartitionWriterService;
+import it.gov.pagopa.mypay2pu.extractor.service.files.CsvService;
+import jakarta.validation.Validator;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DebtPositionsTypeOrgOperatorsExportProcessingService extends BaseExportProcessingService<DebtPositionsTypeOrgOperators, PuDebtPositionsTypeOrgOperatorsDTO> {
+
+  private final DebtPositionsTypeOrgOperatorsDao debtPositionsTypeOrgOperatorsDao;
+  private final DebtPositionsTypeOrgOperatorsMapper debtPositionsTypeOrgOperatorsMapper;
+
+  public DebtPositionsTypeOrgOperatorsExportProcessingService(DebtPositionsTypeOrgOperatorsDao debtPositionsTypeOrgOperatorsDao,
+                                                              DebtPositionsTypeOrgOperatorsMapper debtPositionsTypeOrgOperatorsMapper,
+                                                              CsvService csvService,
+                                                              CsvPartitionWriterService csvPartitionWriterService,
+                                                              FileArchiverService fileArchiverService,
+                                                              Validator validator,
+                                                              ExtractorExportProperties exportProperties) {
+    super(csvService, csvPartitionWriterService, fileArchiverService, validator, exportProperties);
+    this.debtPositionsTypeOrgOperatorsDao = debtPositionsTypeOrgOperatorsDao;
+    this.debtPositionsTypeOrgOperatorsMapper = debtPositionsTypeOrgOperatorsMapper;
+  }
+
+  @Override
+  protected MigrationFileType getMigrationFileType() {
+    return MigrationFileType.DEBT_POSITIONS_TYPE;
+  }
+
+  @Override
+  protected Class<PuDebtPositionsTypeOrgOperatorsDTO> getDtoClass() {
+    return PuDebtPositionsTypeOrgOperatorsDTO.class;
+  }
+
+  @Override
+  protected String getZipVersion() {
+    return PuDebtPositionsTypeOrgOperatorsDTO.VERSION;
+  }
+
+  @Override
+  protected PuDebtPositionsTypeOrgOperatorsDTO toExportableEntity(DebtPositionsTypeOrgOperators debtPositionsTypeOrgOperators) {
+    return debtPositionsTypeOrgOperatorsMapper.map(debtPositionsTypeOrgOperators);
+  }
+
+  @Override
+  protected List<DebtPositionsTypeOrgOperators> retrieveData(ExtractionRequest request, int pageSize, int offset) {
+    ExtractionFilters filters = request.getFilters();
+    return debtPositionsTypeOrgOperatorsDao.findByFilters(
+      request.getIpaCode(),
+      null,
+      null,
+      pageSize,
+      offset
+    );
+  }
+}
