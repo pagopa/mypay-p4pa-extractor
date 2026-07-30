b68ae5fadc2294676beda3f074273965477a34d4
domenicogi <domenico.giacobbe@eng.it>
2026-07-30 10:04:03 +0200

    feat: P4ADEV-4952 debt position mapper (#43)
    Co-authored-by: macacia <marco.cacia@eng.it>

--- build.gradle.kts
+++ build.gradle.kts
@@ -148,7 +148,8 @@ tasks.register("dependenciesBuild") {
 
   dependsOn(
     "openApiGenerate",
-    "openApiGenerate_P4PAORGANIZATION"
+    "openApiGenerate_P4PAORGANIZATION",
+    "openApiGenerate_P4PADEBTPOSITIONS"
   )
 }
 
@@ -218,5 +219,52 @@ tasks.register<GenerateTask>("openApiGenerate_P4PAORGANIZATION") {
     )
   )
   library.set("resttemplate")
+  workerIsolation.set("process")
+}
+
+tasks.register<GenerateTask>("openApiGenerate_P4PADEBTPOSITIONS") {
+  group = "AutomaticallyGeneratedCode"
+  description = "openapi"
+
+  generatorName.set("java")
+  remoteInputSpec.set("https://raw.githubusercontent.com/pagopa/p4pa-doc/refs/heads/main/openapi/main/internal/p4pa-debt-positions.generated.openapi.json")
+  outputDir.set("$projectDir/build/generated")
+  invokerPackage.set("it.gov.pagopa.pu.debtposition.generated")
+  apiPackage.set("it.gov.pagopa.pu.debtposition.client.generated")
+  modelPackage.set("it.gov.pagopa.pu.debtposition.dto.generated")
+  typeMappings.set(
+    mapOf(
+      "LocalDateTime" to "java.time.LocalDateTime",
+      "object" to "tools.jackson.databind.JsonNode",
+      "string+binary" to "Resource",
+      "SpontaneousFormStructure" to "tools.jackson.databind.JsonNode",
+    )
+  )
+  importMappings.set(
+    mapOf(
+      "Resource" to "org.springframework.core.io.Resource"
+    )
+  )
+  configOptions.set(
+    mapOf(
+      "swaggerAnnotations" to "false",
+      "openApiNullable" to "false",
+      "dateLibrary" to "java8",
+      "serializableModel" to "true",
+      "useSpringBoot4" to "true",
+      "useJackson3" to "true",
+      "useJakartaEe" to "true",
+      "useOneOfInterfaces" to "true",
+      "useBeanValidation" to "true",
+      "serializationLibrary" to "jackson",
+      "generateSupportingFiles" to "true",
+      "generateConstructorWithAllArgs" to "true",
+      "generatedConstructorWithRequiredArgs" to "true",
+      "enumPropertyNaming" to "original",
+      "additionalModelTypeAnnotations" to "@lombok.experimental.SuperBuilder(toBuilder = true)"
+    )
+  )
+  library.set("resttemplate")
+
   workerIsolation.set("process")
 }
