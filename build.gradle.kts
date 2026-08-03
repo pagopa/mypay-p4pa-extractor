aa7fe7bd28c512796232bb6f3d8abe87f581809c
dgiacobbe <domenico.giacobbe@eng.it>
2026-08-03 11:56:39 +0200

    P4ADEV-4956: PaymentNotificationMapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -149,7 +149,8 @@ tasks.register("dependenciesBuild") {
   dependsOn(
     "openApiGenerate",
     "openApiGenerate_P4PAORGANIZATION",
-    "openApiGenerate_P4PADEBTPOSITIONS"
+    "openApiGenerate_P4PADEBTPOSITIONS",
+    "openApiGenerate_P4PACLASSIFICATION"
   )
 }
 
@@ -266,5 +267,38 @@ tasks.register<GenerateTask>("openApiGenerate_P4PADEBTPOSITIONS") {
   )
   library.set("resttemplate")
 
+  workerIsolation.set("process")
+}
+
+tasks.register<GenerateTask>("openApiGenerate_P4PACLASSIFICATION") {
+  group = "AutomaticallyGeneratedCode"
+  description = "Generates the classification models used by payment-notification exports."
+
+  generatorName.set("java")
+  remoteInputSpec.set("https://raw.githubusercontent.com/pagopa/p4pa-doc/refs/heads/main/openapi/main/internal/p4pa-classification.generated.openapi.json")
+  outputDir.set("$projectDir/build/generated")
+  invokerPackage.set("it.gov.pagopa.pu.classification.generated")
+  apiPackage.set("it.gov.pagopa.pu.classification.client.generated")
+  modelPackage.set("it.gov.pagopa.pu.classification.dto.generated")
+  configOptions.set(
+    mapOf(
+      "swaggerAnnotations" to "false",
+      "openApiNullable" to "false",
+      "dateLibrary" to "java8",
+      "serializableModel" to "true",
+      "useSpringBoot4" to "true",
+      "useJackson3" to "true",
+      "useJakartaEe" to "true",
+      "useOneOfInterfaces" to "true",
+      "useBeanValidation" to "true",
+      "serializationLibrary" to "jackson",
+      "generateSupportingFiles" to "true",
+      "generateConstructorWithAllArgs" to "true",
+      "generatedConstructorWithRequiredArgs" to "true",
+      "enumPropertyNaming" to "original",
+      "additionalModelTypeAnnotations" to "@lombok.experimental.SuperBuilder(toBuilder = true)"
+    )
+  )
+  library.set("resttemplate")
   workerIsolation.set("process")
 }
---
+++ src/main/java/it/gov/pagopa/mypay2pu/extractor/dto/export/PuPaymentNotificationDTO.java
@@ -0,0 +1,96 @@
+package it.gov.pagopa.mypay2pu.extractor.dto.export;
+
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvDate;
+import it.gov.pagopa.pu.classification.dto.generated.PersonEntityType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(callSuper = false)
+public class PuPaymentNotificationDTO implements CsvExportDto {
+
+  @CsvBindByName(column = "IUD")
+  @NotBlank
+  private String iud;
+
+  @CsvBindByName(column = "codIuv")
+  @NotBlank
+  private String iuv;
+
+  @CsvBindByName(column = "tipoIdentificativoUnivoco")
+  @NotNull
+  private PersonEntityType debtorUniqueIdentifierType;
+
+  @CsvBindByName(column = "codiceIdentificativoUnivoco")
+  @NotBlank
+  private String debtorUniqueIdentifierCode;
+
+  @CsvBindByName(column = "anagraficaPagatore")
+  @NotBlank
+  private String debtorFullName;
+
+  @CsvBindByName(column = "indirizzoPagatore")
+  private String debtorAddress;
+
+  @CsvBindByName(column = "civicoPagatore")
+  private String debtorCivic;
+
+  @CsvBindByName(column = "capPagatore")
+  private String debtorPostalCode;
+
+  @CsvBindByName(column = "localitaPagatore")
+  private String debtorLocation;
+
+  @CsvBindByName(column = "provinciaPagatore")
+  private String debtorProvince;
+
+  @CsvBindByName(column = "nazionePagatore")
+  private String debtorNation;
+
+  @CsvBindByName(column = "e-mailPagatore")
+  private String debtorEmail;
+
+  @CsvBindByName(column = "dataEsecuzionePagamento")
+  @CsvDate(value = "yyyy-MM-dd")
+  @NotNull
+  private LocalDate paymentExecutionDate;
+
+  @CsvBindByName(column = "importoDovutoPagato")
+  @NotNull
+  private BigDecimal amountPaid;
+
+  @CsvBindByName(column = "commissioneCaricoPa")
+  @NotNull
+  private BigDecimal paCommission;
+
+  @CsvBindByName(column = "tipoDovuto")
+  @NotBlank
+  private String debtPositionTypeOrgCode;
+
+  @CsvBindByName(column = "tipoVersamento")
+  @NotBlank
+  private String paymentType;
+
+  @CsvBindByName(column = "causaleVersamento")
+  @NotBlank
+  private String remittanceInformation;
+
+  @CsvBindByName(column = "datiSpecificiRiscossione")
+  @NotBlank
+  private String transferCategory;
+
+  @CsvBindByName(column = "bilancio")
+  private String balance;
+}
