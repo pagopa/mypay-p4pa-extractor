77ef0dafeea8661782b6cea2db2312af25ea57de
domenicogi <domenico.giacobbe@eng.it>
2026-08-04 14:34:09 +0200

    fix: P4ADEV-4881 fix debtpositiontype filename (#52)
    Co-authored-by: macacia <marco.cacia@eng.it>

--- src/test/java/it/gov/pagopa/mypay2pu/extractor/service/export/CsvPartitionWriterServiceTest.java
+++ src/test/java/it/gov/pagopa/mypay2pu/extractor/service/export/CsvPartitionWriterServiceTest.java
@@ -103,6 +103,6 @@ class CsvPartitionWriterServiceTest {
   }
 
   private ExportFileNameBuilder getFileNameBuilder() {
-    return new ExportFileNameBuilder("IPA_CODE", "IPA_CODE_ORG", MigrationFileType.ORGANIZATIONS, LocalDate.now().atStartOfDay(), "1_0");
+    return new ExportFileNameBuilder("IPA_CODE", "IPA_CODE_ORG", true, MigrationFileType.ORGANIZATIONS, LocalDate.now().atStartOfDay(), "1_0");
   }
 }
