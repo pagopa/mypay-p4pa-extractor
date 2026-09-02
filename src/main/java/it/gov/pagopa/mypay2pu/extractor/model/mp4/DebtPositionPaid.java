54fdda498ad4dd943b55cbec433720730b9468fe
dgiacobbe <domenico.giacobbe@eng.it>
2026-09-02 12:23:14 +0200

    P4ADEV-5015 code review: add lombok annotations

--- src/main/java/it/gov/pagopa/mypay2pu/extractor/model/mp4/DebtPositionPaid.java
+++ src/main/java/it/gov/pagopa/mypay2pu/extractor/model/mp4/DebtPositionPaid.java
@@ -1,148 +1,97 @@
 package it.gov.pagopa.mypay2pu.extractor.model.mp4;
 
 import it.gov.pagopa.mypay2pu.extractor.model.ExportModel;
-import org.jspecify.annotations.NonNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Arrays;
 
-public record DebtPositionPaid(
-  String iuf,
-  String codIpaEnte,
-  Long numRigaFlusso,
-  String codIud,
-  String codRpSilinviarpIdUnivocoVersamento,
-  String deEVersioneOggetto,
-  String codEDomIdDominio,
-  String codEDomIdStazioneRichiedente,
-  String codEIdMessaggioRicevuta,
-  LocalDateTime dtEDataOraMessaggioRicevuta,
-  String codERiferimentoMessaggioRichiesta,
-  LocalDateTime codERiferimentoDataRichiesta,
-  Character codEIstitAttIdUnivAttTipoIdUnivoco,
-  String codEIstitAttIdUnivAttCodiceIdUnivoco,
-  String deEIstitAttDenominazioneAttestante,
-  String codEIstitAttCodiceUnitOperAttestante,
-  String deEIstitAttDenomUnitOperAttestante,
-  String deEIstitAttIndirizzoAttestante,
-  String deEIstitAttCivicoAttestante,
-  String codEIstitAttCapAttestante,
-  String deEIstitAttLocalitaAttestante,
-  String deEIstitAttProvinciaAttestante,
-  String codEIstitAttNazioneAttestante,
-  Character codEEnteBenefIdUnivBenefTipoIdUnivoco,
-  String codEEnteBenefIdUnivBenefCodiceIdUnivoco,
-  String deEEnteBenefDenominazioneBeneficiario,
-  String codEEnteBenefCodiceUnitOperBeneficiario,
-  String deEEnteBenefDenomUnitOperBeneficiario,
-  String deEEnteBenefIndirizzoBeneficiario,
-  String deEEnteBenefCivicoBeneficiario,
-  String codEEnteBenefCapBeneficiario,
-  String deEEnteBenefLocalitaBeneficiario,
-  String deEEnteBenefProvinciaBeneficiario,
-  String codEEnteBenefNazioneBeneficiario,
-  Character codESoggVersIdUnivVersTipoIdUnivoco,
-  String codESoggVersIdUnivVersCodiceIdUnivoco,
-  String deESoggVersAnagraficaVersante,
-  String deESoggVersIndirizzoVersante,
-  String deESoggVersCivicoVersante,
-  String codESoggVersCapVersante,
-  String deESoggVersLocalitaVersante,
-  String deESoggVersProvinciaVersante,
-  String codESoggVersNazioneVersante,
-  String deESoggVersEmailVersante,
-  Character codESoggPagIdUnivPagTipoIdUnivoco,
-  String codESoggPagIdUnivPagCodiceIdUnivoco,
-  String codESoggPagAnagraficaPagatore,
-  String deESoggPagIndirizzoPagatore,
-  String deESoggPagCivicoPagatore,
-  String codESoggPagCapPagatore,
-  String deESoggPagLocalitaPagatore,
-  String deESoggPagProvinciaPagatore,
-  String deESoggPagNazionePagatore,
-  String deESoggPagEmailPagatore,
-  Character codEDatiPagCodiceEsitoPagamento,
-  BigDecimal numEDatiPagImportoTotalePagato,
-  String codEDatiPagIdUnivocoVersamento,
-  String codEDatiPagCodiceContestoPagamento,
-  BigDecimal numEDatiPagDatiSingPagSingoloImportoPagato,
-  String deEDatiPagDatiSingPagEsitoSingoloPagamento,
-  LocalDateTime dtEDatiPagDatiSingPagDataEsitoSingoloPagamento,
-  String codEDatiPagDatiSingPagIdUnivocoRiscoss,
-  String deRpDatiVersDatiSingVersCausaleVersamento,
-  String deEDatiPagDatiSingPagDatiSpecificiRiscossione,
-  String codTipoDovuto,
-  String deRtInviartTipoFirma,
-  byte[] blbRtPayload,
-  Integer indiceDatiSingoloPagamento,
-  BigDecimal numEDatiPagDatiSingPagCommissioniApplicatePsp,
-  String codEDatiPagDatiSingPagAllegatoRicevutaTipo,
-  byte[] blbEDatiPagDatiSingPagAllegatoRicevutaTest,
-  String bilancio,
-  String codFiscalePa1,
-  String deNomePa1,
-  String codTassonomicoDovutoPa1,
-  LocalDateTime dtCreazione
-) implements ExportModel {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DebtPositionPaid implements ExportModel {
+
+  private String iuf;
+  private String codIpaEnte;
+  private Long numRigaFlusso;
+  private String codIud;
+  private String codRpSilinviarpIdUnivocoVersamento;
+  private String deEVersioneOggetto;
+  private String codEDomIdDominio;
+  private String codEDomIdStazioneRichiedente;
+  private String codEIdMessaggioRicevuta;
+  private LocalDateTime dtEDataOraMessaggioRicevuta;
+  private String codERiferimentoMessaggioRichiesta;
+  private LocalDateTime codERiferimentoDataRichiesta;
+  private Character codEIstitAttIdUnivAttTipoIdUnivoco;
+  private String codEIstitAttIdUnivAttCodiceIdUnivoco;
+  private String deEIstitAttDenominazioneAttestante;
+  private String codEIstitAttCodiceUnitOperAttestante;
+  private String deEIstitAttDenomUnitOperAttestante;
+  private String deEIstitAttIndirizzoAttestante;
+  private String deEIstitAttCivicoAttestante;
+  private String codEIstitAttCapAttestante;
+  private String deEIstitAttLocalitaAttestante;
+  private String deEIstitAttProvinciaAttestante;
+  private String codEIstitAttNazioneAttestante;
+  private Character codEEnteBenefIdUnivBenefTipoIdUnivoco;
+  private String codEEnteBenefIdUnivBenefCodiceIdUnivoco;
+  private String deEEnteBenefDenominazioneBeneficiario;
+  private String codEEnteBenefCodiceUnitOperBeneficiario;
+  private String deEEnteBenefDenomUnitOperBeneficiario;
+  private String deEEnteBenefIndirizzoBeneficiario;
+  private String deEEnteBenefCivicoBeneficiario;
+  private String codEEnteBenefCapBeneficiario;
+  private String deEEnteBenefLocalitaBeneficiario;
+  private String deEEnteBenefProvinciaBeneficiario;
+  private String codEEnteBenefNazioneBeneficiario;
+  private Character codESoggVersIdUnivVersTipoIdUnivoco;
+  private String codESoggVersIdUnivVersCodiceIdUnivoco;
+  private String deESoggVersAnagraficaVersante;
+  private String deESoggVersIndirizzoVersante;
+  private String deESoggVersCivicoVersante;
+  private String codESoggVersCapVersante;
+  private String deESoggVersLocalitaVersante;
+  private String deESoggVersProvinciaVersante;
+  private String deESoggVersNazioneVersante;
+  private String deESoggVersEmailVersante;
+  private Character codESoggPagIdUnivPagTipoIdUnivoco;
+  private String codESoggPagIdUnivPagCodiceIdUnivoco;
+  private String codESoggPagAnagraficaPagatore;
+  private String deESoggPagIndirizzoPagatore;
+  private String deESoggPagCivicoPagatore;
+  private String codESoggPagCapPagatore;
+  private String deESoggPagLocalitaPagatore;
+  private String deESoggPagProvinciaPagatore;
+  private String deESoggPagNazionePagatore;
+  private String deESoggPagEmailPagatore;
+  private Character codEDatiPagCodiceEsitoPagamento;
+  private BigDecimal numEDatiPagImportoTotalePagato;
+  private String codEDatiPagIdUnivocoVersamento;
+  private String codEDatiPagCodiceContestoPagamento;
+  private BigDecimal numEDatiPagDatiSingPagSingoloImportoPagato;
+  private String deEDatiPagDatiSingPagEsitoSingoloPagamento;
+  private LocalDateTime dtEDatiPagDatiSingPagDataEsitoSingoloPagamento;
+  private String codEDatiPagDatiSingPagIdUnivocoRiscoss;
+  private String deRpDatiVersDatiSingVersCausaleVersamento;
+  private String deEDatiPagDatiSingPagDatiSpecificiRiscossione;
+  private String codTipoDovuto;
+  private String deRtInviartTipoFirma;
+  private byte[] blbRtPayload;
+  private Integer indiceDatiSingoloPagamento;
+  private BigDecimal numEDatiPagDatiSingPagCommissioniApplicatePsp;
+  private String codEDatiPagDatiSingPagAllegatoRicevutaTipo;
+  private byte[] blbEDatiPagDatiSingPagAllegatoRicevutaTest;
+  private String bilancio;
+  private String codFiscalePa1;
+  private String deNomePa1;
+  private String codTassonomicoDovutoPa1;
+  private LocalDateTime dtCreazione;
 
   @Override
   public String logicalKey() {
     return codIud + "|" + codRpSilinviarpIdUnivocoVersamento;
-  }
-
-  @Override
-  public boolean equals(Object object) {
-    if (this == object) {
-      return true;
-    }
-    return object instanceof DebtPositionPaid other
-      && Arrays.deepEquals(values(), other.values());
-  }
-
-  @Override
-  public int hashCode() {
-    return Arrays.deepHashCode(values());
-  }
-
-  @Override
-  @NonNull
-  public String toString() {
-    return "DebtPositionPaid" + Arrays.deepToString(values());
-  }
-
-  private Object[] values() {
-    return new Object[] {
-      iuf, codIpaEnte, numRigaFlusso, codIud, codRpSilinviarpIdUnivocoVersamento,
-      deEVersioneOggetto, codEDomIdDominio, codEDomIdStazioneRichiedente,
-      codEIdMessaggioRicevuta, dtEDataOraMessaggioRicevuta, codERiferimentoMessaggioRichiesta,
-      codERiferimentoDataRichiesta, codEIstitAttIdUnivAttTipoIdUnivoco,
-      codEIstitAttIdUnivAttCodiceIdUnivoco, deEIstitAttDenominazioneAttestante,
-      codEIstitAttCodiceUnitOperAttestante, deEIstitAttDenomUnitOperAttestante,
-      deEIstitAttIndirizzoAttestante, deEIstitAttCivicoAttestante, codEIstitAttCapAttestante,
-      deEIstitAttLocalitaAttestante, deEIstitAttProvinciaAttestante, codEIstitAttNazioneAttestante,
-      codEEnteBenefIdUnivBenefTipoIdUnivoco, codEEnteBenefIdUnivBenefCodiceIdUnivoco,
-      deEEnteBenefDenominazioneBeneficiario, codEEnteBenefCodiceUnitOperBeneficiario,
-      deEEnteBenefDenomUnitOperBeneficiario, deEEnteBenefIndirizzoBeneficiario,
-      deEEnteBenefCivicoBeneficiario, codEEnteBenefCapBeneficiario,
-      deEEnteBenefLocalitaBeneficiario, deEEnteBenefProvinciaBeneficiario,
-      codEEnteBenefNazioneBeneficiario, codESoggVersIdUnivVersTipoIdUnivoco,
-      codESoggVersIdUnivVersCodiceIdUnivoco, deESoggVersAnagraficaVersante,
-      deESoggVersIndirizzoVersante, deESoggVersCivicoVersante, codESoggVersCapVersante,
-      deESoggVersLocalitaVersante, deESoggVersProvinciaVersante, codESoggVersNazioneVersante,
-      deESoggVersEmailVersante, codESoggPagIdUnivPagTipoIdUnivoco,
-      codESoggPagIdUnivPagCodiceIdUnivoco, codESoggPagAnagraficaPagatore,
-      deESoggPagIndirizzoPagatore, deESoggPagCivicoPagatore, codESoggPagCapPagatore,
-      deESoggPagLocalitaPagatore, deESoggPagProvinciaPagatore, deESoggPagNazionePagatore,
-      deESoggPagEmailPagatore, codEDatiPagCodiceEsitoPagamento, numEDatiPagImportoTotalePagato,
-      codEDatiPagIdUnivocoVersamento, codEDatiPagCodiceContestoPagamento,
-      numEDatiPagDatiSingPagSingoloImportoPagato, deEDatiPagDatiSingPagEsitoSingoloPagamento,
-      dtEDatiPagDatiSingPagDataEsitoSingoloPagamento, codEDatiPagDatiSingPagIdUnivocoRiscoss,
-      deRpDatiVersDatiSingVersCausaleVersamento, deEDatiPagDatiSingPagDatiSpecificiRiscossione,
-      codTipoDovuto, deRtInviartTipoFirma, blbRtPayload, indiceDatiSingoloPagamento,
-      numEDatiPagDatiSingPagCommissioniApplicatePsp, codEDatiPagDatiSingPagAllegatoRicevutaTipo,
-      blbEDatiPagDatiSingPagAllegatoRicevutaTest, bilancio, codFiscalePa1, deNomePa1,
-      codTassonomicoDovutoPa1, dtCreazione
-    };
   }
 }
