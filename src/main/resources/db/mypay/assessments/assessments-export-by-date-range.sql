4a737ff603ba8a7c61f6802136db8525555f49f2
macacia <marco.cacia@eng.it>
2026-09-03 18:30:50 +0200

    queries

---
+++ src/main/resources/db/mypay/assessments/assessments-export-by-date-range.sql
@@ -0,0 +1,19 @@
+SELECT
+    a.de_nome_accertamento AS assessment_name,
+    ad.cod_ipa_ente AS organization_ipa_code,
+    ad.cod_tipo_dovuto AS debt_position_type_org_code,
+    ad.cod_iuv AS iuv,
+    ad.cod_iud AS iud,
+    ad.cod_ufficio AS office_code,
+    NULL AS office_description,
+    ad.cod_capitolo AS section_code,
+    NULL AS section_description,
+    ad.cod_accertamento AS assessment_code,
+    NULL AS assessment_description,
+    CAST(ad.num_importo * 100 AS BIGINT) AS amount_cents,
+    ad.flg_importo_inserito AS amount_submitted
+FROM mygov_accertamento a
+JOIN mygov_accertamento_dettaglio ad ON ad.mygov_accertamento_id = a.mygov_accertamento_id
+WHERE ad.cod_ipa_ente = :codIpaEnte
+  AND a.dt_ultima_modifica BETWEEN :dateFrom AND :dateTo
+ORDER BY a.de_nome_accertamento, ad.cod_iuv;
