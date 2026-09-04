58047e476e196089e92c2a0741c4ccc21920b22d
macacia <marco.cacia@eng.it>
2026-09-04 11:50:33 +0200

    PuAssessmentsDTO with Lombok annotations and VERSION static field

--- src/main/java/it/gov/pagopa/mypay2pu/extractor/dto/export/PuAssessmentsDTO.java
+++ src/main/java/it/gov/pagopa/mypay2pu/extractor/dto/export/PuAssessmentsDTO.java
@@ -3,62 +3,60 @@ package it.gov.pagopa.mypay2pu.extractor.dto.export;
 import com.opencsv.bean.CsvBindByName;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class PuAssessmentsDTO implements CsvExportDto {
+  public static final String VERSION = "1.0";
 
-    @CsvBindByName(column = "nomeAccertamento")
-    @NotBlank
-    private String assessmentName;
+  @CsvBindByName(column = "nomeAccertamento")
+  @NotBlank
+  private String assessmentName;
 
-    @CsvBindByName(column = "enteIpaCode")
-    private String organizationIpaCode;
+  @CsvBindByName(column = "enteIpaCode")
+  private String organizationIpaCode;
 
-    @CsvBindByName(column = "codiceEnteTipoDovuto")
-    @NotBlank
-    private String debtPositionTypeOrgCode;
+  @CsvBindByName(column = "codiceEnteTipoDovuto")
+  @NotBlank
+  private String debtPositionTypeOrgCode;
 
-    @CsvBindByName(column = "iuv")
-    @NotBlank
-    private String iuv;
+  @CsvBindByName(column = "iuv")
+  @NotBlank
+  private String iuv;
 
-    @CsvBindByName(column = "iud")
-    @NotBlank
-    private String iud;
+  @CsvBindByName(column = "iud")
+  @NotBlank
+  private String iud;
 
-    @CsvBindByName(column = "codUfficio")
-    private String officeCode;
+  @CsvBindByName(column = "codUfficio")
+  private String officeCode;
 
-    @CsvBindByName(column = "descrizioneCodUfficio")
-    private String officeDescription;
+  @CsvBindByName(column = "descrizioneCodUfficio")
+  private String officeDescription;
 
-    @CsvBindByName(column = "codCapitolo")
-    @NotBlank
-    private String sectionCode;
+  @CsvBindByName(column = "codCapitolo")
+  @NotBlank
+  private String sectionCode;
 
-    @CsvBindByName(column = "descrizioneCodCapitolo")
-    private String sectionDescription;
+  @CsvBindByName(column = "descrizioneCodCapitolo")
+  private String sectionDescription;
 
-    @CsvBindByName(column = "codiceAccertamento")
-    private String assessmentCode;
+  @CsvBindByName(column = "codiceAccertamento")
+  private String assessmentCode;
 
-    @CsvBindByName(column = "descrizioneCodiceAccertamento")
-    private String assessmentDescription;
+  @CsvBindByName(column = "descrizioneCodiceAccertamento")
+  private String assessmentDescription;
 
-    @CsvBindByName(column = "importoCentesimi")
-    @NotBlank
-    private Long amountCents;
+  @CsvBindByName(column = "importoCentesimi")
+  @NotBlank
+  private Long amountCents;
 
-    @CsvBindByName(column = "importoVersato")
-    @NotNull
-    private Boolean amountSubmitted;
+  @CsvBindByName(column = "importoVersato")
+  @NotNull
+  private Boolean amountSubmitted;
 
 }
-
