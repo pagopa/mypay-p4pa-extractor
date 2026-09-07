f21ce3d64492840606051c8c677911316af57aeb
macacia <marco.cacia@eng.it>
2026-09-07 16:41:06 +0200

    add mapper

---
+++ src/main/java/it/gov/pagopa/mypay2pu/extractor/mapper/assessmentsregistry/AssessmentsRegistryMapper.java
@@ -0,0 +1,24 @@
+package it.gov.pagopa.mypay2pu.extractor.mapper.assessmentsregistry;
+
+import it.gov.pagopa.mypay2pu.extractor.dto.export.PuAssessmentsRegistryDTO;
+import it.gov.pagopa.mypay2pu.extractor.model.mpv4.AssessmentsRegistry;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AssessmentsRegistryMapper {
+
+  public PuAssessmentsRegistryDTO map(AssessmentsRegistry model) {
+    return PuAssessmentsRegistryDTO.builder()
+        .organizationIpaCode(model.organizationIpaCode())
+        .debtPositionTypeOrgCode(model.debtPositionTypeOrgCode())
+        .sectionCode(model.sectionCode())
+        .sectionDescription(model.sectionDescription())
+        .officeCode(model.officeCode())
+        .officeDescription(model.officeDescription())
+        .assessmentCode(model.assessmentCode())
+        .assessmentDescription(model.assessmentDescription())
+        .operatingYear(model.operatingYear())
+        .status(model.status())
+        .build();
+  }
+}
